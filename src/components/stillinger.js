51816cc939ff3e3acd7e44d67499bdb7840515b2
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-12 20:17:12 +0100

    Gjorde stillingsvisning dynamisk

--- src/components/stillinger.js
+++ src/components/stillinger.js
@@ -1,4 +1,36 @@
-const Stillinger = () => {
+import Stilling from "./stilling";
+
+const capitalize = (word) => {
+  if (typeof word !== "string") return "";
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const Gruppe = ({ stillingsType, stillinger }) => {
+  const typeStillinger = stillinger.filter((stilling) => stilling.stillingsType === stillingsType);
+  if (typeStillinger.length === 0) {
+    return null;
+  }
+  const typeHeader = `col col-12 col-md-10 col-lg-8 stilling-header ${stillingsType}-bg px-0`;
+  return (
+    <>
+      <div className="row mt-5 justify-content-center mx-0">
+        <div className={typeHeader}>
+          <h3 className="m-0">{capitalize(stillingsType)}</h3>
+        </div>
+      </div>
+
+      <div className="row justify-content-center">
+        <ul className="col col-12 col-md-10 col-lg-8 list-unstyled">
+          {typeStillinger.map((stilling) => (
+            <Stilling key={stilling.id} {...stilling} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+const Stillinger = ({ stillinger }) => {
   return (
     <>
       <div className="row my-4">
@@ -9,122 +41,9 @@ const Stillinger = () => {
         </div>
       </div>
 
-      <div className="row mt-5 justify-content-center mx-0">
-        <div className="col col-12 col-md-10 col-lg-8 stilling-header utvikling-bg px-0">
-          <h3 className="m-0">Utvikling</h3>
-        </div>
-      </div>
-
-      <div className="row justify-content-center">
-        <ul className="col col-12 col-md-10 col-lg-8 list-unstyled">
-          <li className="stilling">
-            <i>Frist: 22.11.2020</i>
-            <h4>
-              <a
-                href="https://arbeidsplassen.nav.no/stillinger/stilling/656d9e26-0e6c-433c-84a3-b6e973f734f6"
-                target="_blank"
-              >
-                Velferdsstatens kommende pensjonister trenger engasjerte utviklere
-              </a>
-            </h4>
-            <p className="lead">
-              Er du en utvikler som mener at den beste måten å møte fremtiden på er å skape den selv? Vil du ved hjelp
-              av feedback og læring lage stadig enklere løsninger for brukerne våre?
-            </p>
-            <a
-              className="navLink"
-              href="https://arbeidsplassen.nav.no/stillinger/stilling/656d9e26-0e6c-433c-84a3-b6e973f734f6"
-              target="_blank"
-            >
-              Les mer om stillingen
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="row mt-5 justify-content-center mx-0">
-        <div className="col col-12 col-md-10 col-lg-8 stilling-header design-bg px-0">
-          <h3 className="m-0">Design</h3>
-        </div>
-      </div>
-      <div className="row justify-content-center">
-        <ul className="col col-12 col-md-10 col-lg-8 list-unstyled">
-          <li className="stilling">
-            <i>Frist: 15.11.2020</i>
-            <h4>
-              <a
-                href="https://arbeidsplassen.nav.no/stillinger/stilling/63cb1b7f-d41d-40f6-bcc1-bb3f0840ae7f"
-                target="_blank"
-              >
-                Er du en samfunnsengasjert student som ønsker å bygge erfaring som tjenestedesigner?
-              </a>
-            </h4>
-            <p className="lead">
-              Vil du ha en sommerjobb i 2021 hvor du kan gjøre en stor forskjell for våre brukere og samtidig jobbe med
-              spennende og ny teknologi? I NAV IT bygger vi nå opp et av de sterkeste designmiljøene i Norge og vi
-              ønsker å få med deg på laget sommeren 2021 for at du skal utvikle deg og få et innblikk i alt det
-              spennende vi holder på med.
-            </p>
-            <a
-              className="navLink"
-              href="https://arbeidsplassen.nav.no/stillinger/stilling/63cb1b7f-d41d-40f6-bcc1-bb3f0840ae7f"
-              target="_blank"
-            >
-              Les mer om stillingen
-            </a>
-          </li>
-
-          <li className="stilling">
-            <i>Frist: 15.11.2020</i>
-            <h4>
-              <a
-                href="https://arbeidsplassen.nav.no/stillinger/stilling/26f1f2ce-924e-4fab-9be7-ae2723248b01"
-                target="_blank"
-              >
-                Er du en samfunnsengasjert student som ønsker å bygge erfaring som design-researcher?
-              </a>
-            </h4>
-            <p className="lead">
-              Vil du ha en sommerjobb i 2021 hvor du kan gjøre en stor forskjell for våre brukere og samtidig jobbe med
-              spennende og ny teknologi? I NAV IT bygger vi nå opp et av de sterkeste designmiljøene i Norge og vi
-              ønsker å få med deg på laget sommeren 2021 for at du skal utvikle deg og få et innblikk i alt det
-              spennende vi holder på med.
-            </p>
-            <a
-              className="navLink"
-              href="https://arbeidsplassen.nav.no/stillinger/stilling/26f1f2ce-924e-4fab-9be7-ae2723248b01"
-              target="_blank"
-            >
-              Les mer om stillingen
-            </a>
-          </li>
-
-          <li className="stilling">
-            <i>Frist: 15.11.2020</i>
-            <h4>
-              <a
-                href="https://arbeidsplassen.nav.no/stillinger/stilling/160742cc-57fd-49e6-9221-afe56437854b"
-                target="_blank"
-              >
-                Er du en samfunnsengasjert student som ønsker å bygge erfaring som utvikler?
-              </a>
-            </h4>
-            <p className="lead">
-              Vil du ha en sommerjobb i 2021 hvor du kan gjøre en stor forskjell for våre brukere og samtidig jobbe med
-              spennende og ny teknologi? I NAV IT bygger vi nå opp et av de sterkeste designmiljøene i Norge og vi
-              ønsker å få med deg på laget sommeren 2021 for at du skal utvikle deg og få et innblikk i alt det
-              spennende vi holder på med.
-            </p>
-            <a
-              className="navLink"
-              href="https://arbeidsplassen.nav.no/stillinger/stilling/160742cc-57fd-49e6-9221-afe56437854b"
-              target="_blank"
-            >
-              Les mer om stillingen
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Gruppe stillinger={stillinger} stillingsType={"utvikling"} />
+      <Gruppe stillinger={stillinger} stillingsType={"design"} />
+      <Gruppe stillinger={stillinger} stillingsType={"analyse"} />
     </>
   );
 };
