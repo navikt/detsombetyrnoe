d240416c760832f1e02b3b43029cb473f2fefb16
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-16 16:56:19 +0100

    legger til litt mer tjafs for at styled-components skal funke smertefritt https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js

---
+++ src/pages/document.tsx
@@ -0,0 +1,36 @@
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import { NextPageContext } from "next";
+import { RenderPage } from "next/dist/next-server/lib/utils";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    return await renderServersideStyledComponentsStylesheet(ctx);
+  }
+}
+
+// https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+async function renderServersideStyledComponentsStylesheet(ctx: NextPageContext & { renderPage: RenderPage }) {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+}
