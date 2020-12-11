515c8f4b2fe49ddb8754eb8fa5db8610f531d264
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-11 20:33:33 +0100

    fix handlerbug

--- src/utils/useInViewport.ts
+++ src/utils/useInViewport.ts
@@ -4,8 +4,7 @@ function useInViewport<T extends HTMLElement>(ref: React.RefObject<T>) {
   const [inViewPort, setInViewport] = useState(false);
 
   useEffect(() => {
-    const top = ref.current?.getBoundingClientRect().top;
-    const handler = () => setInViewport(top ? top < window.innerHeight : true);
+    const handler = () => setInViewport((ref.current?.getBoundingClientRect().top || Infinity) < window.innerHeight);
     handler();
     window.addEventListener("scroll", handler);
     return () => window.removeEventListener("scroll", handler);
