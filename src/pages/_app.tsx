1d2fc22f98f6faa08bf191fa1f60e8aab025d829
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-11 20:53:01 +0100

    flytter Headers og felles ting fra index til _app.tsx, der det skal ligge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { AmplitudeProvider } from "../contexts/amplitude-context";
 import "../styles/globals.css";
+import { ResetCSS } from "../styles/reset.css";
+import { GlobalStyle } from "../styles/GlobalStyle";
+import Metadata from "../components/Metadata";
+import React from "react";
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <AmplitudeProvider>
-      <Component {...pageProps} />
-    </AmplitudeProvider>
+    <>
+      <ResetCSS />
+      <GlobalStyle />
+      <Metadata />
+      <AmplitudeProvider>
+        <Component {...pageProps} />
+      </AmplitudeProvider>
+    </>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
 import Legacy from "../legacy/static-with-positions/Legacy";
 import React from "react";
-import Metadata from "../components/Metadata";
-import { ResetCSS } from "../styles/reset.css";
-import { GlobalStyle } from "../styles/GlobalStyle";
 
 export default function Home() {
-  return (
-    <>
-      <ResetCSS />
-      <GlobalStyle />
-      <Metadata />
-      <Legacy />
-    </>
-  );
+  return <Legacy />;
 }
