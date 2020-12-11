cf37b925c46b3005716ba4701a78ffd277eb3edd
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-11 20:36:04 +0100

    flytter global styling og reset.css til index.tsx så det blir med overalt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Legacy from "../legacy/static-with-positions/Legacy";
 import React from "react";
 import Metadata from "../components/Metadata";
+import { ResetCSS } from "../styles/reset.css";
+import { GlobalStyle } from "../styles/GlobalStyle";
 
 export default function Home() {
   return (
     <>
+      <ResetCSS />
+      <GlobalStyle />
       <Metadata />
       <Legacy />
     </>
--- src/pages/nyttdesign.tsx
+++ src/pages/nyttdesign.tsx
@@ -2,18 +2,14 @@ import * as React from "react";
 import styled from "styled-components";
 import Panel from "../components/Panel";
 import { navFrontend } from "../styles/navFarger";
-import { GlobalStyle } from "../styles/GlobalStyle";
 import Githubstats from "./githubstats";
 import Nøkkeltall from "../components/nøkkeltall/Nøkkeltall";
-import { ResetCSS } from "../styles/reset.css";
 
 const Style = styled.div``;
 
 function NyttDesign() {
   return (
     <Style>
-      <ResetCSS />
-      <GlobalStyle />
       <Panel backgroundColor={navFrontend.navBlaDarken40} fontColor="white">
         Hei på deg
       </Panel>
