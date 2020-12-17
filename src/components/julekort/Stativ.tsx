4b7e7a625edadc066195c7d032c5680b494af0f9
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-17 13:12:58 +0100

    easteregg

--- src/components/julekort/Stativ.tsx
+++ src/components/julekort/Stativ.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
-import styled from "styled-components/macro";
+import styled, { css } from "styled-components/macro";
 import { navFrontend } from "../../styles/navFarger";
 import { keyframes } from "styled-components/macro";
 import { delay } from "./animasjoner";
 import Sokk from "./ikoner/Sokk";
+import { useState } from "react";
+import { SiTypescript } from "react-icons/all";
 
 const rotateIn = keyframes`
 from {
@@ -47,21 +49,53 @@ const dropDown = keyframes`
   }
 `;
 
-const StyledSokk = styled(Sokk)`
-  animation: ${dropDown} 0.5s ${delay + 3}s backwards;
-  transform-origin: top;
-  height: 2em;
+type SokkProps = { offset: number; delay: number; mirror?: boolean; onClick?: () => void; pointer?: boolean };
+
+const SokkStyle = styled.div<SokkProps>`
   position: absolute;
-  bottom: 1.5em;
-  left: 3.5em;
-  fill: ${navFrontend.navRod};
+  bottom: 1.4em;
+  left: ${(props) => props.offset}em;
+  ${(props) =>
+    props.mirror &&
+    css`
+      transform: rotateY(180deg);
+    `};
+  svg {
+    animation: ${dropDown} 0.5s ${(props) => props.delay}s both;
+    transform-origin: top;
+    height: 2em;
+    fill: ${navFrontend.navRod};
+    ${(props) =>
+      props.pointer &&
+      css`
+        cursor: pointer;
+      `};
+  }
 `;
 
+function SokkKomponent(props: SokkProps) {
+  return (
+    <SokkStyle {...props}>
+      <Sokk />
+    </SokkStyle>
+  );
+}
+
 function Stativ() {
+  const [easterEgg, setEasterEgg] = useState(false);
+
   return (
     <Style>
       <Tråd />
-      <StyledSokk />
+      <SokkKomponent offset={3.2} onClick={() => setEasterEgg(true)} delay={delay + 3} pointer={!easterEgg} />
+      {easterEgg && (
+        <>
+          <SokkKomponent offset={0.1} delay={0} />
+          <SokkKomponent offset={1.6} delay={0.4} mirror={true} />
+          <SokkKomponent offset={4.5} delay={0.3} />
+          <SokkKomponent offset={6} delay={0.8} mirror={true} />
+        </>
+      )}
     </Style>
   );
 }
