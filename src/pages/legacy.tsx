da3f44adbf96206f0c6b5800a1f8b1f0ee9349ee
Daniel Winsvold <danielwinsvold@gmail.com>
2021-01-21 14:00:57 +0100

    sletter legacy-filer som ikke lenger er i bruk

--- src/pages/legacy.tsx
+++
@@ -1,6 +0,0 @@
-import Legacy from "../legacy/static-with-positions/Legacy";
-import React from "react";
-
-export default function Home() {
-  return <Legacy />;
-}
--- src/utils/serverUrl.ts
+++
@@ -1,3 +0,0 @@
-const dev = process.env.NODE_ENV !== "production";
-
-export const serverUrl = dev ? "http://localhost:3000" : "https://www.detsombetyrnoe.no";
