61db22196f9a884bc92510b4af70f2bcc395856d
Daniel Winsvold <danielwinsvold@gmail.com>
2021-01-25 12:24:42 +0100

    skrur av useCdn for å se om det fikser livepreviews for drafts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -9,7 +9,6 @@ import {
 const config: ClientConfig = {
   dataset: "production",
   projectId: "c9hptfq7",
-  useCdn: process.env.NODE_ENV === "production",
 };
 
 export const urlFor = (source: any) => createImageUrlBuilder(config).image(source);
