679be715e114b048c99d3be82b369efda324e9e9
Daniel Winsvold <danielwinsvold@gmail.com>
2021-01-25 12:33:02 +0100

    Revert "skrur av useCdn for å se om det fikser livepreviews for drafts"
    This reverts commit 61db22196f9a884bc92510b4af70f2bcc395856d.

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -9,6 +9,7 @@ import {
 const config: ClientConfig = {
   dataset: "production",
   projectId: "c9hptfq7",
+  useCdn: process.env.NODE_ENV === "production",
 };
 
 export const urlFor = (source: any) => createImageUrlBuilder(config).image(source);
