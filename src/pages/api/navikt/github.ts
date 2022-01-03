b647179b0c0a8724b924dbf339b3472f634de3f5
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-01-03 10:35:56 +0100

    Bruker riktig origin for cors

--- src/pages/api/navikt/github.ts
+++ src/pages/api/navikt/github.ts
@@ -5,7 +5,7 @@ import { query } from "../../../api-utils/github/query";
 const apiHandler: NextApiHandler = async (request, response) => {
   try {
     const data = await client.query({ query });
-    response.setHeader("Access-Control-Allow-Origin", "https://navikt.github.com");
+    response.setHeader("Access-Control-Allow-Origin", "https://navikt.github.io");
     response.status(200).json(data.data);
   } catch (error) {
     response.status(500).json("Det skjedde en feil: " + JSON.stringify(error));
