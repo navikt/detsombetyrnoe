c0a43ad0404721261622c69ee4a6a139658a1264
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-01-27 12:17:24 +0100

    Legger inn config-fil for security headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const securityHeaders = require("./security-headers-config");
+
 const STUDIO_REWRITE = {
   source: "/studio/:path*",
   destination: process.env.NODE_ENV === "development" ? "http://localhost:3333/studio/:path*" : "/studio/index.html",
@@ -23,5 +25,14 @@ module.exports = {
   experimental: {
     // Enables the styled-components SWC transform
     styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
---
+++ security-headers-config.js
@@ -0,0 +1,8 @@
+const headers = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+];
+
+module.exports = headers;
