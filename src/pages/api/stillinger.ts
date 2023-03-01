4e851eaea3fb13b6fc5b030a3a221407f97d4a93
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2023-03-01 09:12:59 +0100

    :fire: Fjerner gammel kode relatert til stillingssøk

--- src/pages/api/stillinger.ts
+++
@@ -1,7 +0,0 @@
-const getStillinger = require("../../lib/get-stillinger");
-
-export default async (request: any, response: any) => {
-  const stillinger = await getStillinger();
-  response.statusCode = 200;
-  response.json(stillinger);
-};
