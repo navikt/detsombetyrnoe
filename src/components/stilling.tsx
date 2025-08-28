d0c627bfc19d680cf8d5567c2ab75aa85a5b0d7d
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2025-08-28 10:22:58 +0200

    feat: add more serverside anonymous metrics

--- src/components/stilling.tsx
+++ src/components/stilling.tsx
@@ -6,7 +6,6 @@ import { CardItem } from "./CardItem";
 import { format } from "date-fns";
 import { nb } from "date-fns/locale";
 import { useInViewport } from "react-in-viewport";
-import metrics from "src/lib/metrics";
 import { clickStillingClient, showStillingClient } from "src/lib/clientMetrics";
 
 const StyledCardItem = styled(CardItem)`
